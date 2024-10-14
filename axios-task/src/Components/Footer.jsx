a63e893098b503d3aae31434b618443ac3c00dd3
renukakathirvel <renukakathirvel18@gmail.com>
2024-10-14 13:11:20 +0530

    first commit

---
+++ axios-task/src/Components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer(props) {
+  return (
+    <footer className="bg-dark text-light py-4">
+      <div className="container">
+        <div className="row justify-content-center align-items-center">
+          <div className="col-md-6">
+            <p className="mb-0">
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
